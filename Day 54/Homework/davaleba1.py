cbd9d49a4da76310c192b1d91b096a4a4d4244e6
Sacred-solos <thesacredninja129@gmail.com>
2024-04-24 22:03:12 +0400

    second commit

---
+++ Day 54/Homework/davaleba1.py
@@ -0,0 +1,7 @@
+a = int(input("Please input your first number: ") ) 
+b = int(input("Please input your second number: ") )
+
+print(a+b)
+print(a-b)
+print(a/b) 
+print(a*b) 
---
+++ Day 55/script.js
@@ -0,0 +1,15 @@
+function mathFormulas(num){  //vqmnit functions
+    console.log(num*2)   //vaketebt num-ze matematikur formulebs
+    console.log(num/2)
+    console.log(num*num)
+}
+
+mathFormulas(5)  //gamogvaqvs funqcia
+
+
+const firstName = prompt("What is your name?")
+const surname = prompt("What is your surname?")
+const age = prompt("What is your age?")
+const address = prompt("What is your address?")
+
+console.log("you are",firstName,surname,"you are ",age,"years old and you live in",address)
